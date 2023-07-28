68744c8838f27fb109ed8ab515bfaaf1154175c4
NickLinOUS <nicklinous1212@gmail.com>
2023-07-28 17:35:14 +0800

    [ 畫面修改 ] 新增電子書，測試 RWD

--- components/file/files/Testdeep.vue
+++ components/file/files/Testdeep.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>123</div>
+   <div style="width: 100%;height: 100vh;">
+          <iframe
+          style="width: 100%; height: 100%;"
+      src="https://online.fliphtml5.com/igxbs/jatu/"
+      seamless="seamless"
+      scrolling="no"
+      frameborder="0"
+      allowtransparency="true"
+      allowfullscreen="true"
+    ></iframe>  
+    </div>
 </template>
 
 <script>
