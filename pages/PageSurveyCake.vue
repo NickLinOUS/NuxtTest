e6c65a708de2de3022390c7bbe6a205fc34c02aa
NickLinOUS <nicklinous1212@gmail.com>
2023-08-03 16:23:33 +0800

    #

--- pages/PageSurveyCake.vue
+++ pages/PageSurveyCake.vue
@@ -2,7 +2,7 @@
   <div style="width: 100%; height: 100%">
     <iframe
       width="100%"
-      height="600px"
+      height="1000px"
       src="https://www.surveycake.com/s/YmR6A"
       style="border: #ddd 1px solid"
       allowTransparency="true"
